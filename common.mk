baacd8adcced4c9c27c6b1e3630ae453d6b98fd1
airk000 <airk908@gmail.com>
2013-04-10 19:33:20 +0800

    Unlock fps for c8812/u8818
    Change-Id: I155430b1d27101004a3b98c2cc168de26041a7f7

--- common.mk
+++ common.mk
@@ -64,7 +64,8 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     device/huawei/msm7x27a-common/prebuilt/BCM4330.hcd:system/etc/bluetooth/BCM4330.hcd \
-    device/huawei/msm7x27a-common/configs/init.bcm.bt.sh:system/etc/bluetooth/init.bcm.bt.sh
+    device/huawei/msm7x27a-common/configs/init.bcm.bt.sh:system/etc/bluetooth/init.bcm.bt.sh \
+    device/huawei/msm7x27a-common/configs/89uncapfps:system/etc/init.d/89uncapfps
 
 PRODUCT_COPY_FILES += \
     device/huawei/msm7x27a-common/configs/audio_policy.conf:system/etc/audio_policy.conf \
